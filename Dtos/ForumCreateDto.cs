2313e69449a9cf288488d6b6e433d6ec82349aa3
printfKrzysztof <krzysztofbortnowski@outlook.com>
2023-08-19 20:44:50 +0200

    Init

---
+++ Dtos/ForumCreateDto.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ForumsService.Dtos
+{
+    public class ForumCreateDto
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        public int DogId { get; set; }
+    }
+}
---
+++ Models/Forum.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ForumsService.Models
+{
+    public class Forum
+    {
+        [Key]
+         [Required]
+        public int Id { get; set; }
+         [Required]
+        public string Title { get; set; }
+         [Required]
+        public string Text { get; set; }
+         [Required]
+        public int UserId{ get; set; }
+        [Required]
+        public DateTime Time {get; set; }
+        public int DogId{ get; set; }
+        public User User { get; set; }
+    }
+}
